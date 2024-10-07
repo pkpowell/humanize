9fc90ca7023f4d47f39bb595cc827fdc4e40b859
phil <p@pkpowell.net>
2024-10-07 09:33:28 +0200

    updates

--- hubytes/humanize.go
+++ hubytes/humanize.go
@@ -8,7 +8,10 @@ import (
 
 type Byter int64
 
-type ByteUnit int
+// 1000 or 1024
+type Unit int
+
+// decimal places - 1, 2, 3...
 type DecimalCount int
 
 const (
@@ -20,24 +23,31 @@ const (
 )
 
 type ByteOptions struct {
-	Unit           ByteUnit     // IEC or SI
+	Unit           Unit         // IEC or SI
 	MaxDecimals    DecimalCount // decimal places without trailing zeros
 	ShowByteLetter bool         // Show trailing 'b' (bytes)
+	Full           bool         // Show full name
+}
+
+type ByteUnit struct {
+	Letter string
+	Name   string
 }
 
 const (
 	// Binary - 1024 bytes in a kilobyte, i.e. 1KiB
-	IEC ByteUnit = 1024
+	IEC Unit = 1024
 
 	// Decimal - 1000 bytes in a kilobyte, i.e. 1KB
-	SI ByteUnit = 1000
+	SI Unit = 1000
 )
 
 // Default options
 var Options = &ByteOptions{
-	Unit:           IEC,  // Binary
-	MaxDecimals:    One,  // 1 decimal place
-	ShowByteLetter: true, // Show trailing 'b'
+	Unit:           IEC,   // binary - 1024 based
+	MaxDecimals:    One,   // 1 decimal place
+	ShowByteLetter: true,  // show trailing 'b'
+	Full:           false, // print Full name
 }
 
 func (o *ByteOptions) ByteLetter() string {
@@ -47,12 +57,34 @@ func (o *ByteOptions) ByteLetter() string {
 	return ""
 }
 
-var Prefix = map[ByteUnit][]string{
+var Prefix = map[Unit][]ByteUnit{
 	// Binary - 1024 bytes in a kilobyte
-	IEC: {"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei"},
+	IEC: {
+		{Letter: "", Name: "byte"},
+		{Letter: "Ki", Name: "kibibyte"},
+		{Letter: "Mi", Name: "mebibyte"},
+		{Letter: "Gi", Name: "gibibyte"},
+		{Letter: "Ti", Name: "tebibyte"},
+		{Letter: "Pi", Name: "pebibyte"},
+		{Letter: "Ei", Name: "exbibyte"},
+		{Letter: "Zi", Name: "zebibyte"},
+		{Letter: "Yi", Name: "yobibyte"},
+	},
 
 	// Decimal - 1000 bytes in a kilobyte
-	SI: {"", "K", "M", "G", "T", "P", "E"},
+	SI: {
+		{Letter: "", Name: "byte"},
+		{Letter: "k", Name: "kilobyte"},
+		{Letter: "M", Name: "megabyte"},
+		{Letter: "G", Name: "gigabyte"},
+		{Letter: "T", Name: "terabyte"},
+		{Letter: "P", Name: "petabyte"},
+		{Letter: "E", Name: "exabyte"},
+		{Letter: "Z", Name: "zettabyte"},
+		{Letter: "Y", Name: "yottabyte"},
+		{Letter: "R", Name: "ronnabyte"},
+		{Letter: "Q", Name: "quettabyte"},
+	},
 }
 
 // Binary
@@ -61,30 +93,43 @@ var iec, _ = decimal.New(int64(IEC), 0)
 // Decimal
 var si, _ = decimal.New(int64(SI), 0)
 
-var Divisor = map[ByteUnit]decimal.Decimal{
+// value container
+var value decimal.Decimal
+var p ByteUnit
+
+var Divisor = map[Unit]decimal.Decimal{
 	// Binary
 	IEC: iec,
 	// Decimal
 	SI: si,
 }
 
-func (b *ByteUnit) Prefix() []string {
+func (b *Unit) Prefix() []ByteUnit {
 	return Prefix[*b]
 }
 
-func (b *ByteUnit) Divisor() decimal.Decimal {
+func (b *Unit) Divisor() decimal.Decimal {
 	return Divisor[*b]
 }
 
 func (s Byter) String() string {
-	value, _ := decimal.New(int64(s), 0)
+	value, _ = decimal.New(int64(s), 0)
 
-	var p string
 	for _, p = range Options.Unit.Prefix() {
 		if value.Less(Options.Unit.Divisor()) {
 			break
 		}
 		value, _ = value.Quo(Options.Unit.Divisor())
 	}
-	return fmt.Sprintf("%s %s%s", value.Trunc(int(Options.MaxDecimals)).Trim(0), p, Options.ByteLetter())
+
+	value = value.Trunc(int(Options.MaxDecimals)).Trim(0)
+
+	if Options.Full {
+		if value.Equal(decimal.One) {
+			return fmt.Sprintf("%s %s", value, p.Name)
+		}
+		return fmt.Sprintf("%s %s", value, p.Name+"s")
+	}
+
+	return fmt.Sprintf("%s%s%s", value, p.Letter, Options.ByteLetter())
 }
--- hubytes/humanize_test.go
+++ hubytes/humanize_test.go
@@ -7,32 +7,24 @@ import (
 var byteValues = []int64{
 	23,
 	1000,
-	1001,
 	1024,
 	1321,
 	2000,
-	2001,
 	2048,
+	1000 * 1000 * 1000,
+	1024 * 1024 * 1024,
 	6000,
 	6001,
 	21325,
 	243202,
 	1839405,
 	18394053,
-	1000 * 1000 * 1000,
-	1024 * 1024 * 1024,
 }
-
-// func TestHumanize(t *testing.T) {
-// 	for _, v := range byteValues {
-// 		fmt.Println(Byter(v))
-// 		t.Log(v, Byter(v))
-// 	}
-// }
 
 func BenchmarkHumanizeIEC(b *testing.B) {
 	Options.Unit = IEC
 	Options.MaxDecimals = One
+	Options.Full = false
 	for range b.N {
 		for _, v := range byteValues {
 			b.Log(v, Byter(v))
@@ -41,14 +33,17 @@ func BenchmarkHumanizeIEC(b *testing.B) {
 	}
 }
 
+var container string
+
 func BenchmarkHumanizeSI(b *testing.B) {
 	Options.Unit = SI
 	Options.MaxDecimals = Two
+	Options.Full = false
 	// b.Log("Decimals: ", Options.MaxDecimals)
 	for range b.N {
 		for _, v := range byteValues {
-			// _ = Byter(v)
-			b.Log(v, Byter(v))
+			container = Byter(v).String()
+			// b.Log(v, Byter(v))
 		}
 	}
 }
